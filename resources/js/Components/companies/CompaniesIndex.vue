921919eb8cf6058596d452520c6e4b103da8fcb5
bahaeddine <sihassi.bahaeddine@gmail.com>
2023-05-30 20:42:58 +0100

    ref

--- resources/js/Components/companies/CompaniesIndex.vue
+++ resources/js/Components/companies/CompaniesIndex.vue
@@ -1,3 +1,54 @@
 <template>
-    Hello world.
+    <div class="min-w-full overflow-hidden overflow-x-auto align-middle sm:rounded-md">
+        <table class="min-w-full border divide-y divide-gray-200">
+            <thead>
+            <tr>
+                <th class="px-6 py-3 bg-gray-50">
+                    <span
+                        class="text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase">Name</span>
+                </th>
+                <th class="px-6 py-3 bg-gray-50">
+                    <span
+                        class="text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase">Email</span>
+                </th>
+                <th class="px-6 py-3 bg-gray-50">
+                    <span
+                        class="text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase">Address</span>
+                </th>
+                <th class="px-6 py-3 bg-gray-50">
+                    <span
+                        class="text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase">Website</span>
+                </th>
+            </tr>
+            </thead>
+
+            <tbody class="bg-white divide-y divide-gray-200 divide-solid">
+            <template v-for="item in companies" :key="item.id">
+                <tr class="bg-white">
+                    <td class="px-6 py-4 text-sm leading-5 text-gray-900 whitespace-no-wrap">
+                        {{ item.name }}
+                    </td>
+                    <td class="px-6 py-4 text-sm leading-5 text-gray-900 whitespace-no-wrap">
+                        {{ item.email }}
+                    </td>
+                    <td class="px-6 py-4 text-sm leading-5 text-gray-900 whitespace-no-wrap">
+                        {{ item.address }}
+                    </td>
+                    <td class="px-6 py-4 text-sm leading-5 text-gray-900 whitespace-no-wrap">
+                        {{ item.website }}
+                    </td>
+                </tr>
+            </template>
+            </tbody>
+        </table>
+    </div>
 </template>
+
+<script setup>
+import useCompanies from '@/composables/companies'
+import { onMounted } from 'vue';
+
+const { companies, getCompanies } = useCompanies()
+
+onMounted(getCompanies)
+</script>
